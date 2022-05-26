1b651e3ab335e1e9fda0bc81870c4c49d65ecb3a
Erika <erikac.ec80@gmail.com>
2022-05-26 16:55:00 -0300

    config firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyBs5hO5wi_SHBJXZk37tc7-9-TiqKZqh28",
+    authDomain: "control-clientes-9ac65.firebaseapp.com",
+    projectId: "control-clientes-9ac65",
+    storageBucket: "control-clientes-9ac65.appspot.com",
+    messagingSenderId: "960141593008",
+    appId: "1:960141593008:web:03d59c930ce78f24f82e75",
+    measurementId: "G-PPB4D27V9S"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore:{
+    apiKey: "AIzaSyBs5hO5wi_SHBJXZk37tc7-9-TiqKZqh28",
+  authDomain: "control-clientes-9ac65.firebaseapp.com",
+  projectId: "control-clientes-9ac65",
+  storageBucket: "control-clientes-9ac65.appspot.com",
+  messagingSenderId: "960141593008",
+  appId: "1:960141593008:web:03d59c930ce78f24f82e75",
+  measurementId: "G-PPB4D27V9S"
+  }
 };
 
 /*
