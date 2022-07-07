536a8e3522d1bb6e4429b29413dba4ce283760fa
Erika <erikac.ec80@gmail.com>
2022-07-07 11:36:11 -0300

    cree getsaldototal

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -21,4 +21,14 @@ export class ClientesComponent implements OnInit {
     )
   }
 
+  getSaldoTotal(){
+    let saldoTotal: number = 0;
+    if(this.clientes){
+      this.clientes.forEach(cliente =>{
+        saldoTotal += Number(cliente.saldo);
+      })
+    }
+    return saldoTotal;
+  }
+
 }
