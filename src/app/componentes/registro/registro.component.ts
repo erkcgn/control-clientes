19d9ea9613ebadaa80fd010217ca31a078e818a1
Erika <erikac.ec80@gmail.com>
2022-07-19 16:06:04 -0300

    copie atributos y servicios del constr + metodo ngOnInit

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  
+  email!: string;
+  password!: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private flashMessages: FlashMessagesService,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+    // metodo cuando se carga el componente
+    this.loginService.getAuth().subscribe(auth => {
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  registro(){
+    
   }
 
 }
