565078930c87d778aa802af5ef32c6edf68e9204
Erika <erikac.ec80@gmail.com>
2022-07-19 16:39:17 -0300

    agregue propiedad permitirRegistro y cree metodo guardar

--- src/app/componentes/configuracion/configuracion.component.ts
+++ src/app/componentes/configuracion/configuracion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracion.component.css']
 })
 export class ConfiguracionComponent implements OnInit {
+  
+  permitirRegistro = false;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  
+  guardar(){
+    
+  }
 }
