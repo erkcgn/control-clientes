4a5790d83b6342eaf9689617fb836e2306d5f63e
Erika <erikac.ec80@gmail.com>
2022-07-19 16:56:23 -0300

    cree metodo getconfiguracion y modificarconfiguracion

--- src/app/servicios/configuracion.service.ts
+++ src/app/servicios/configuracion.service.ts
@@ -1,5 +1,24 @@
-import { AngularFirestoreDocument } from "@angular/fire/compat/firestore";
+import { AngularFirestore, AngularFirestoreDocument } from "@angular/fire/compat/firestore";
+import { Observable } from "rxjs";
+import { Configuracion } from "../modelo/configuracion.model";
 
 export class ConfiguracionServicio{
-    configuracionDoc!: AngularFirestoreDocument;
+    configuracionDoc!: AngularFirestoreDocument<Configuracion>;
+    configuracion!: Observable<Configuracion>;
+
+    //id unico de la coleccion de configuracion
+    id = '1';
+
+    constructor(private db: AngularFirestore){}
+
+    getConfiguracion(): Observable<Configuracion>{
+        this.configuracionDoc = this.db.doc<Configuracion>(`configuracion/${this.id}`);
+        this.configuracion = this.configuracionDoc.valueChanges() as any;
+        return this.configuracion; //del objeto que es un observable
+    }
+
+    modificarConfiguracion(configuracion: Configuracion){
+        this.configuracionDoc = this.db.doc<Configuracion>(`configuracion/${this.id}`);
+        this.configuracionDoc.update(configuracion);
+    }
 }
