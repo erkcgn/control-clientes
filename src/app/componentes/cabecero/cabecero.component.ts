d9cff54b2042467f7f0e939ec4b3fce194b39eb7
Erika <erikac.ec80@gmail.com>
2022-07-19 11:11:20 -0300

    cree atributos para saber si el uss esta logueado y agregue servicios al constructor

--- src/app/componentes/cabecero/cabecero.component.ts
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-cabecero',
@@ -7,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceroComponent implements OnInit {
  
+  //nuestro usuario esta logueado?
+  isLoggedIn!: boolean;
+  loggedInUser!: string; 
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    //inicializamos nuestro login user
+    
   }
 
 }
